d26faa3ea54b5c3c746a102144b6f56b78e99232
Lucas Mello <lucasdemello.18@gmail.com>
2021-05-23 16:09:55 -0300

    JogoV1

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour    
+{
+    public float VelRot;
+
+    public GameObject[] locais;
+    public int destinoInicial = 0;
+    public float velocidade = 0;
+    public bool comecarInvertido;
+    public bool reiniciarSequencia;
+
+    int localAtual = 0;
+    bool inverter = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (destinoInicial < locais.Length)
+        {
+            localAtual = destinoInicial;
+        }
+        else
+        {
+            localAtual = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, VelRot * Time.deltaTime, 0);
+
+        if (inverter == false)
+        {
+            if (Vector3.Distance(transform.position, locais[localAtual].transform.position) < 0.1f)
+            {
+                if (localAtual < locais.Length - 1)
+                {
+                    localAtual++;
+                }
+                else
+                {
+                    if (reiniciarSequencia == true)
+                    {
+                        localAtual = 0;
+                    }
+                    else
+                    {
+                        inverter = true;
+                    }
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, locais[localAtual].transform.position, velocidade * Time.deltaTime);
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, locais[localAtual].transform.position) < 0.1f)
+            {
+                if (localAtual > 0)
+                {
+                    localAtual--;
+                }
+                else
+                {
+                    if (reiniciarSequencia == true)
+                    {
+                        localAtual = locais.Length - 1;
+                    }
+                    else
+                    {
+                        inverter = false;
+                    }
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, locais[localAtual].transform.position, velocidade * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float turnSpeed;
+
+    private float verticalInput;
+    private float horizontalInput;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * verticalInput);
+
+        if (Mathf.Abs(verticalInput) > 0) {
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        }        
+
+    }
+}
